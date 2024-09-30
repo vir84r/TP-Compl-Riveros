f93c9a860fcde884e28679b9bc6d2406d6b63b95
vir84nr@gmail.com <vir84nr@gmail.com>
2024-09-30 15:10:14 -0300

    Agregar archivos de proyecto.

---
+++ Estrategia.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using tp1;
+
+namespace tpfinal
+{
+
+    public class Estrategia
+    {
+
+        public String Consulta1(List<Proceso> datos)
+        {
+            string resutl = "Implementar";
+
+            return resutl;
+
+        }
+
+        public String Consulta2(List<Proceso> datos)
+        {
+            string resutl = "Implementar";
+
+            return resutl;
+        }
+
+
+
+        public String Consulta3(List<Proceso> datos)
+        {
+            string resutl = "Implementar";
+
+            return resutl;
+        }
+
+
+        public void ShortesJobFirst(List<Proceso> datos, List<Proceso> collected)
+        {
+
+            // Implementar
+            
+        }
+
+
+        public void PreemptivePriority(List<Proceso> datos, List<Proceso> collected)
+        {
+           
+        	// Implementar
+        	
+        }
+        
+    }
+}
